8f220dd991e0c051204f51d76672ec11bec59582
itscopebd <itscopebd@gmail.com>
2023-06-19 14:53:33 +0600

    Navbar styles and content added

--- src/components/Announcement/Announcement.jsx
+++ src/components/Announcement/Announcement.jsx
@@ -7,14 +7,14 @@ const Announcement = () => {
           <div className='flex justify-between items-center container mx-auto text-white'>
             <div className='flex gap-5'>
                 <p> <strong className='font-primary font-medium'>Contact: </strong> Pabna, Santhia </p>
-                <p> <strong>Call Us: </strong> 01580495093 </p>
+                <p> <strong className='font-primary font-medium'>Call Us: </strong> 01580495093 </p>
 
             </div>
             <div className='flex gap-5'>
-              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'>  <FaFacebookF className= 'text-black text-lg hover:text-primary'></FaFacebookF></li>
-              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaTwitter className= 'text-black text-lg hover:text-primary'></FaTwitter></li>
-              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaGoogle className= 'text-black text-lg hover:text-primary'></FaGoogle></li>
-              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaYoutube className= 'text-black text-lg hover:text-primary'></FaYoutube></li>
+              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'>  <FaFacebookF className= 'text-black text-xs hover:text-primary'></FaFacebookF></li>
+              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaTwitter className= 'text-black text-xs hover:text-primary'></FaTwitter></li>
+              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaGoogle className= 'text-black text-xs hover:text-primary'></FaGoogle></li>
+              <li className='w-6 h-6 hover:text-primary cursor-pointer rounded-full bg-white flex justify-center items-center'><FaYoutube className= 'text-black text-xs hover:text-primary'></FaYoutube></li>
               
             </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Announcement from '../components/Announcement/announcement';
+import NavBar from '../components/NavBar/NavBar';
 
 const Home = () => {
     return (
         <div>
             <Announcement></Announcement>
+            <NavBar></NavBar>
         </div>
     );
 };
