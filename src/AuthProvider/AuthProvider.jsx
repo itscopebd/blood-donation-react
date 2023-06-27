3486971a779ddd43d9c27d10423451e514019ed4
itscopebd <itscopebd@gmail.com>
2023-06-27 08:31:30 -0700

    user logout and user dashboard conditionaly show

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,12 +1,13 @@
 
-import { createContext, useState } from 'react';
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from 'react';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../firebase/Firebase';
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({ children }) => {
     const auth = getAuth(app)
     const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null)
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -28,16 +29,29 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
 
-    const user = {
-        name: "Rofiq",
+    const userOut=()=>{
+        return signOut(auth)
+    }
+
+    const userInfo = {
+        user,
         createUser,
         UpdateUserProfile,
         singInWithGoogle,
-        loginWithPasswordAndEmail
+        loginWithPasswordAndEmail,
+        userOut
     }
 
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user)
+        })
+        return unsubscribe()
+    }, [])
+
     return (
-        <AuthContext.Provider value={user}>
+        <AuthContext.Provider value={userInfo}>
             {children}
         </AuthContext.Provider>
     );
